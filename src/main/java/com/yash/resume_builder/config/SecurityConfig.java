6d6c5b8fef943735160b73575e0e1777493b81d2
Pasan-Yashobha <pasanyashobha.254@gmail.com>
2026-04-25 21:16:38 +0530

    fix: add Vercel frontend URL to CORS

--- src/main/java/com/yash/resume_builder/config/SecurityConfig.java
+++ src/main/java/com/yash/resume_builder/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
 
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(("http://localhost:5173")));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://resumora-frontend.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
